44af41e66c360caa3f4d3b5a9b5d09e4bf99ed62
heleledd <157613131+heleledd@users.noreply.github.com>
2025-03-27 21:03:41 +0000

    get statement as text for csv

--- bank-statement-script.py
+++ bank-statement-script.py
@@ -5,7 +5,9 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import logging
 
-base_url = 'https://api.starlingbank.com'
+bank_api_url = 'https://api.starlingbank.com'
+google_sheets_api_url = 'https://sheets.googleapis.com'
+spreadsheet_id = ...
 
 class StarlingBankAPI:
     def __init__(self, token, cert_path):
@@ -18,13 +20,11 @@ class StarlingBankAPI:
         # self._s.verify = cert_path
         
         # make request to get accountUid
-        response = self._s.get(base_url + '/api/v2/accounts')
+        response = self._s.get(bank_api_url + '/api/v2/accounts')
         data = response.json()
         self.accountUid = data['accounts'][0]['accountUid']
 
 def main():
-    # Subtract one month from the current date
-    # date format should be YYYY-MM-DD
     # Get the current date as a datetime object
     current_date = datetime.now()
 
@@ -38,16 +38,42 @@ def main():
     logging.debug("One month ago:", one_month_ago_str)
     
     # GET bank statement for past month from starling bank using their api
-    url = base_url + '/api/v2/accounts/{{accountUid}}/statement/downloadForDateRange'
+    url = bank_api_url + f'/api/v2/accounts/{bank_api.accountUid}/statement/downloadForDateRange'
     params = {
-        'start': one_month_ago,
-        'end': current_date
+        'start': one_month_ago_str,
+        'end': current_date_str
     }
     
-    response = bank_api._s.get(url, params)
+    # set accept header to get pdf file
+    bank_api._s.headers.update({'Accept': 'text/csv'})
     
-    print('i want to see the response...')
+    response = bank_api._s.get(url, params=params)
+    
+    if response.status_code == 200:
+        # Extract the CSV content as text
+        csv_content = response.text
+        print(csv_content)  # Print the CSV content or process it further
+    else:
+        logging.error(f"Failed to fetch the statement. Status code: {response.status_code}")
+        logging.error(f"Response: {response.text}")
+    
+    # GOOGLE SHEETS API - write the csv content to a google sheet
+    # authenticate with google sheets api using oauth2
+    
     # format it into a nice excel/csv file / maybe find out if this file could be persistent so that you add a sheet to it every time?
+    url = google_sheets_api_url + f'/v4/spreadsheets/{spreadsheet_id}:batchUpdate'
+    
+    response = bank_api._s.post(url, json={
+        'requests': [
+            {
+                'addSheet': {
+                    'properties': {
+                        'title': f"Statement {current_date_str}"
+                    }
+                }
+            }
+        ]
+    })
     
     # e-mail to EMAIL_ADDRESS whenever the script is run
 
